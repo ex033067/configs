d0cf64342ba95208b82622b98e7482c5366fc724
viniciusban <viniciusban@gmail.com>
2016-02-29 09:47:38 -0300

    Remove mistaken `local` to create `$FILE` var

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ ln -sf ${PWD}/files/git_commit_template.txt ~/.git_commit_template.txt
 
 if test "$(uname)" = "Darwin"
 then
-    local FILE=${PWD}/files/gitconfig_os_specific__Darwin
+    FILE=${PWD}/files/gitconfig_os_specific__Darwin
 else
-    local FILE=${PWD}/files/gitconfig_os_specific__Linux
+    FILE=${PWD}/files/gitconfig_os_specific__Linux
 fi
 
 ln -sf $FILE ~/.gitconfig_os_specific
