1f3032291a1cb36acd7cecbd2d49a86d0befaea1
viniciusban <viniciusban@gmail.com>
2018-10-01 11:02:48 -0300

    Enter project, given partial name

--- bashenv.d/functions/p.sh
+++ bashenv.d/functions/p.sh
@@ -2,5 +2,25 @@
 
 unset -f p
 function p () {
-    cd ${PROJECT_ROOT}/$1
+    local dir=${PROJECT_ROOT}/$1
+    if [ -d "$dir" ]; then
+        cd ${PROJECT_ROOT}/$1
+        return
+    fi
+
+    local find="find ${PROJECT_ROOT} -maxdepth 1 -name '$1*' -type d"
+    local possibilities=$(eval $find | wc -l)
+    if [ "$possibilities" = '0' ]; then
+        echo 'No project found'
+        return 1
+    elif [ "$possibilities" = '1' ]; then
+        echo "entering"
+        cd "$(eval $find)"
+        return
+    else
+        echo 'More then one project found'
+        echo "$(eval $find)"
+        return 1
+    fi
+
 }
