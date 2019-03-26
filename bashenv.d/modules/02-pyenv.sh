5364bb3bd0ade999e6d9d015a49a004e48cf30aa
viniciusban <viniciusban@gmail.com>
2019-03-26 13:10:05 -0300

    Update Python version for pyenv

--- bashenv.d/modules/02-pyenv.sh
+++ bashenv.d/modules/02-pyenv.sh
@@ -11,5 +11,5 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 if which pyenv > /dev/null
 then
     eval "$(pyenv init -)"
-    pyenv global 3.6.5 2.7.14 tools jupyter
+    pyenv global 3.7.2 2.7.14 tools jupyter
 fi
