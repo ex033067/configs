aaa31c5ee4a1a9b68cfe0b4d292fa3fe6d3d9f38
viniciusban <viniciusban@gmail.com>
2020-04-22 17:46:24 -0300

    Fix installation

--- install.sh
+++ install.sh
@@ -9,14 +9,7 @@ XDG_DATA_HOME=${XDG_DATA_HOME:-~/.local/share}
 
 NEOVIM_CONFIG_DIR=${XDG_CONFIG_HOME}/nvim
 NEOVIM_DATA_DIR=${XDG_DATA_HOME}/nvim
-NEOVIM_PLUGINS_DIR=${NEOVIM_DATA_DIR}/vim-plug
 
-rm -f ${NEOVIM_CONFIG_DIR}
+rm -f ${NEOVIM_CONFIG_DIR}/*
 ln -sf $PWD ${NEOVIM_CONFIG_DIR}
-ln -sf $PWD ~/.vimrc
-mkdir --parent ${NEOVIM_PLUGINS_DIR}
-
-
-# Download vim-plug to an autoload directory
-curl -fLo ${NEOVIM_DATA_DIR}/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf $PWD ~/.vim
