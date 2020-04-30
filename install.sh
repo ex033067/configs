41af23eeafae445b0a3faf918a8fb41bc768f385
viniciusban <viniciusban@gmail.com>
2020-04-30 19:32:51 -0300

    Install in MacOS too

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ -r ~/.bash_aliases ]]; then
-	mv ~/.bash_aliases ~/.bash_aliases.$(date +%Y-%m-%d.%H-%M)
+if [[ "$(uname)" = "Darwin" ]]; then
+    filename=~/.bashrc
+else
+    filename=~/.bash_aliases
 fi
 
-ln -s ${PWD}/bashrc ~/.bash_aliases
+if [[ -r ${filename} ]]; then
+    mv ${filename} ${filename}.$(date +%Y-%m-%d.%H-%M)
+fi
+ln -s ${PWD}/bashrc ${filename}
+
 ln -s ${PWD}/ps1_colors.example ~/.ps1_colors
 ln -s ${PWD}/ps1_hostname_alias.example ~/.ps1_hostname_alias
-ln -s ${PWD}/pyenv_global_command.example ~/.pyenv_global_command 
+ln -s ${PWD}/pyenv_global_command.example ~/.pyenv_global_command
