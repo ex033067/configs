87349335e472768b649a1bba624ea7353aed308d
Vini <viniciusban@gmail.com>
2023-05-02 20:12:36 +0100

    [neovim] Add custom config to pylsp

--- vim/luascripts/lsp_config.lua
+++ vim/luascripts/lsp_config.lua
@@ -27,6 +27,17 @@ require("lspconfig").bashls.setup {
 -- }
 
 require("lspconfig").pylsp.setup {
-  on_attach = on_attach
+  on_attach = on_attach,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'}
+          -- ignore = {'W391'},
+          -- maxLineLength = 88
+        }
+      }
+    }
+  }
 }
 
