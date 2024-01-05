79eb93f809efc10e602c00691d8b119a86ebea2e
Vini <viniciusban@gmail.com>
2024-01-05 05:44:14 +0000

    [nvim] Create mapping to toggle preview in telescope pickers

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,4 +4,27 @@ require('telescope').setup {
       hide_on_startup = true
     }
   },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+        },
+      },
+    },
+    find_files = {
+      mappings = {
+        i = {
+          ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+        },
+      },
+    },
+    live_grep = {
+      mappings = {
+        i = {
+          ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+        },
+      },
+    },
+  },
 }
