935fe1a485710324733defb0d79dca6144790337
Vini <viniciusban@gmail.com>
2024-02-23 17:25:36 +0000

    [nvim] Format list of required treesitter parser

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "comment"},
+  -- A list of parser names, or "all" (the first five should always be installed)
+  ensure_installed = {
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
